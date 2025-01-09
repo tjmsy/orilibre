d36d01d36f4f02aac6c7de0ab117a11a65b9e26e
tjmsy <18753632+tjmsy@users.noreply.github.com>
2025-01-10 01:35:10 +0900

    feat: add terrainParameterControl

--- docs/japan/main.js
+++ docs/japan/main.js
@@ -2,35 +2,22 @@ import { isomizer } from "https://cdn.jsdelivr.net/gh/tjmsy/maplibre-gl-isomizer
 import { ScaleRatioControl } from "https://cdn.jsdelivr.net/gh/tjmsy/maplibre-gl-scale-ratio@latest/src/maplibre-gl-scale-ratio.js";
 import { MagneticNorthControl } from "https://cdn.jsdelivr.net/gh/tjmsy/maplibre-gl-magnetic-north/src/maplibre-gl-magnetic-north.js";
 import GPSTrackControl from "https://cdn.jsdelivr.net/gh/tjmsy/maplibre-gl-gps-track/src/maplibre-gl-gps-track.js";
+import terrainParameterControl from "https://cdn.jsdelivr.net/gh/tjmsy/orilibre-utils/src/controls/terrainParameterControl.js";
 
 async function main() {
   try {
-    const demSource = await new mlcontour.DemSource({
-      url: "https://gsj-seamless.jp/seamless/elev/php/terrainRGB.php?url=https://tiles.gsj.jp/tiles/elev/mixed/{z}/{y}/{x}.png",
-      encoding: "mapbox",
-      maxzoom: 14,
-      worker: true,
-      cacheSize: 100,
-      timeoutMs: 20000,
-    });
-    demSource.setupMaplibre(maplibregl);
-
     const mapConfig = {
       container: "map",
       center: [139.3127, 35.8855],
       zoom: 15,
+      maxPitch: 80,
+      hash: true,
     };
 
     const map = await isomizer(
       mapConfig,
       "https://cdn.jsdelivr.net/gh/tjmsy/isomizer-projectfiles/projects/isomized-japan/project-config.yml"
     );
-
-    map.addSource("terrain-source", {
-      type: "raster-dem",
-      tiles: [demSource.sharedDemProtocolUrl],
-      tileSize: 256,
-    });
     return map;
   } catch (error) {
     console.error("Error initializing map:", error);
@@ -62,11 +49,6 @@ map.addControl(
   "top-right"
 );
 map.addControl(new GPSTrackControl({isHeartRateWidthEnabled: true}), "top-right");
-map.addControl(
-  new maplibregl.TerrainControl({
-    source: "terrain-source",
-    exaggeration: 1.2,
-  })
-);
+map.addControl(new terrainParameterControl(), "top-right");
 map.addControl(new maplibregl.NavigationControl(), "bottom-right");
 map.addControl(new maplibregl.ScaleControl({ unit: "metric" }), "bottom-left");
