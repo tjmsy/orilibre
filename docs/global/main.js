7c96173fd37f7d944fb2a340ef1e85f3b44a74f2
tjmsy <18753632+tjmsy@users.noreply.github.com>
2025-09-23 18:50:14 +0900

    Migrated all map data to open data sources; printing and export now unrestricted

--- docs/global/main.js
+++ docs/global/main.js
@@ -2,35 +2,22 @@ import { isomizer } from "https://cdn.jsdelivr.net/gh/tjmsy/maplibre-gl-isomizer
 import { ScaleRatioControl } from "https://cdn.jsdelivr.net/gh/tjmsy/maplibre-gl-scale-ratio@latest/src/maplibre-gl-scale-ratio.js";
 import { MagneticNorthControl } from "https://cdn.jsdelivr.net/gh/tjmsy/maplibre-gl-magnetic-north/src/maplibre-gl-magnetic-north.js";
 import GPSTrackControl from "https://cdn.jsdelivr.net/gh/tjmsy/maplibre-gl-gps-track/src/maplibre-gl-gps-track.js";
+import terrainParameterControl from "https://cdn.jsdelivr.net/gh/tjmsy/orilibre-utils/src/controls/terrainParameterControl.js";
 
 async function main() {
   try {
-    const demSource = await new mlcontour.DemSource({
-      url: "https://tjmsy.azurewebsites.net/api/terrain-rgb/{z}/{x}/{y}.webp",
-      encoding: "mapbox",
-      maxzoom: 14,
-      worker: true,
-      cacheSize: 100,
-      timeoutMs: 10000,
-    });
-    demSource.setupMaplibre(maplibregl);
-
     const mapConfig = {
       container: "map",
       center: [0, 0],
       zoom: 1,
+      maxPitch: 80,
+      hash: true,
     };
 
     const map = await isomizer(
       mapConfig,
       "https://cdn.jsdelivr.net/gh/tjmsy/isomizer-projectfiles/projects/global/project-config.yml"
     );
-
-    map.addSource("terrain-source", {
-      type: 'raster-dem',
-      tiles: [demSource.sharedDemProtocolUrl],
-      tileSize: 256
-    });
     return map;
   } catch (error) {
     console.error("Error initializing map:", error);
@@ -49,6 +36,12 @@ map.addControl(new maplibregl.GeolocateControl({
   showUserHeading: true
 }), 'top-right');
 map.addControl(
+  new MaplibreExportControl.MaplibreExportControl({
+    PrintableArea: true,
+  }),
+  "top-right"
+);
+map.addControl(
   new MagneticNorthControl({
     apiProxyUrl:
       "https://apiproxymagneticnorth.azurewebsites.net/api/getMagneticHeading",
@@ -56,11 +49,6 @@ map.addControl(
   "top-right"
 );
 map.addControl(new GPSTrackControl({isHeartRateWidthEnabled: true}), "top-right");
-map.addControl(
-  new maplibregl.TerrainControl({
-    source: "terrain-source",
-    exaggeration: 1.5,
-  })
-);
+map.addControl(new terrainParameterControl(), "top-right");
 map.addControl(new maplibregl.NavigationControl(), "bottom-right");
 map.addControl(new maplibregl.ScaleControl({ unit: "metric" }), "bottom-left");
