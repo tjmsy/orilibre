87b395d3eecfa836f0a50ffb49d98282c45308c9
tjmsy <18753632+tjmsy@users.noreply.github.com>
2026-05-12 03:01:16 +0900

    refactor: change control version

--- docs/main.js
+++ docs/main.js
@@ -6,7 +6,7 @@ import GeoJsonExportControl from "https://cdn.jsdelivr.net/gh/tjmsy/maplibre-gl-
 import Terrain3dToggle from "https://cdn.jsdelivr.net/gh/tjmsy/maplibre-gl-terrain-3d-toggle@0.1/src/maplibre-gl-terrain-3d-toggle.js";
 import ContourIntervalControl from "https://cdn.jsdelivr.net/gh/tjmsy/maplibre-gl-contour-interval@0.1/src/maplibre-gl-contour-interval.js";
 import StyleScratchpadControl from "https://cdn.jsdelivr.net/gh/tjmsy/maplibre-gl-style-scratchpad@0.1/src/StyleScratchpadControl.js";
-import DesignSetSwitcherControl from "https://cdn.jsdelivr.net/gh/tjmsy/orilibre-utils@0.1/src/orilibre-design-set-switcher/DesignSetSwitcherControl.js";
+import DesignSetSwitcherControl from "https://cdn.jsdelivr.net/gh/tjmsy/orilibre-utils@0.2/src/orilibre-design-set-switcher/DesignSetSwitcherControl.js";
 
 const query = new URLSearchParams(window.location.search);
 
